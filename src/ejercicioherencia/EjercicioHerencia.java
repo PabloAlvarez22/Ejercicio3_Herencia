8c926ffd3bd5ca7ddab0908cb5c5e393e736bff2
PabloAlvarez22 <pabloalv567@gmail.com>
2022-09-26 12:06:23 -0600

    Principio del menú

--- src/ejercicioherencia/EjercicioHerencia.java
+++ src/ejercicioherencia/EjercicioHerencia.java
@@ -23,12 +23,29 @@ public class EjercicioHerencia {
     }
     
     /*
-     * Método pensado para crear un menú interactivo por consola
+     * Método utilizado para crear un menú interactivo.
      * */    
     
     public static  void menu(){
        
-                
-            }
-       
+    	int respuesta=0;
+        while(respuesta!=12){
+            System.out.println("BIENVENIDOO A LA BIBLOTECA UVG");
+            System.out.println("ELIGE UNA OPCIÓN");
+            System.out.println("1. agregar libro");
+            System.out.println("2. agregar revista");
+            System.out.println("3. agregar articulo");
+            System.out.println("4. agregar cliente");
+            System.out.println("5. ver documentos");
+            System.out.println("6. ver clientes");
+            System.out.println("7. ver cantidad de documentos según materia");
+            System.out.println("8. ver cantidad de revista según materia");
+            System.out.println("9. solicitar libro");
+            System.out.println("10. solicitar revista");
+            System.out.println("11. solicitar articulo");
+            System.out.println("12. SALIR");
+            respuesta= entrada.nextInt();       
+        }
+    }
 }
+      
